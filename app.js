75111f097882be856673a62e07ba542ddecae8e1
Dottenpixel <dev@dougmolidor.com>
2015-02-09 18:05:34 -0800

    Handling query string in request

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var http = require('http'),
+    url = require('url')
     fs = require('fs')
 var port = process.env.PORT || 3000
 http.createServer(function(req, res) {
-    var url = './' + (req.url == '/' ? 'index.html' : req.url)
-    fs.readFile(url, function(err, html) {
+    console.log(url.parse(req.url,true));
+    var req_url = url.parse(req.url,true);
+    var file_url = './build/' + (req_url.pathname == '/' ? 'index.html' : req_url.pathname)
+    fs.readFile(file_url, function(err, html) {
         if (err) {
             var message404 = "There is no such page! <a href='/'>Back to home page</a>"
             res.writeHead(404, {'Content-Type': 'text/html', 'Content-Length': message404.length})
